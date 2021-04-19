ff3a46588042ea61aa88f74c53b5052fa4442d86
ejezie <shady07chinex@gmail.com>
2021-04-19 23:46:45 +0100

    set viewport to no scalable

--- spotifyme-app/src/App.js
+++ spotifyme-app/src/App.js
@@ -12,4 +12,4 @@ function App() {
 
 export default App;
 
-// 817f21565c8d409d8df7ee8b6a1a670e
+// 
--- spotifyme-app/src/spotifyme.js
+++ spotifyme-app/src/spotifyme.js
@@ -1,1 +1,3 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
+const redirectUri = "http://localhost:3000";
+const clientId = "817f21565c8d409d8df7ee8b6a1a670e";
