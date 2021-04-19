7372dd0dfef7cd1202a446b776ab36708915dd4c
ejezie <shady07chinex@gmail.com>
2021-04-20 00:38:29 +0100

    adding scopes

--- spotifyme-app/src/spotifyme.js
+++ spotifyme-app/src/spotifyme.js
@@ -1,3 +1,13 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
+
 const redirectUri = "http://localhost:3000";
+
 const clientId = "817f21565c8d409d8df7ee8b6a1a670e";
+
+const scope = [
+    "useri-read-current-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+]
