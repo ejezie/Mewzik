7920d72be1de65147fc58feb46290577f05bf078
ejezie <shady07chinex@gmail.com>
2021-04-21 22:33:30 +0100

    styling player components

--- spotifyme-app/src/Sidebar.jsx
+++ spotifyme-app/src/Sidebar.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./Sidebar.css";
 import logo from "./images/mewzik.PNG";
+import SidebarOption from "./SidebarOption";
+
+
+
 
 function Sidebar() {
   return (
     <div className="sidebar">
       <img src={logo} alt="logo" className="logo" />
+      <SidebarOption title="Home" />
+      <SidebarOption title="Search" />
+      <SidebarOption title="Your library" />
     </div>
   );
 }
