ef115e2cd8bee99047b37069ebff5afabbd942e6
ejezie <shady07chinex@gmail.com>
2021-04-28 16:18:40 +0100

    change redirect uri

--- spotifyme-app/src/spotifyme.js
+++ spotifyme-app/src/spotifyme.js
@@ -1,6 +1,6 @@
 export const authEndpoint = "https://accounts.spotify.com/authorize";
 
-const redirectUri = "http://localhost:3000/";
+const redirectUri = "https://mewzik.vercel.app/";
 
 const clientId = "6bea638450f54befb2b5ff467a5ba989";
 
